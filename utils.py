13b141f3265cbcfaaf081cd981375373950dbc9b
小萌新 <3209970865@qq.com>
2025-05-30 16:44:47 +0800

    Fix: gp color

--- utils.py
+++ utils.py
@@ -7,19 +7,19 @@ def get_pref():
 
 def get_tool_prop(context):
     """bpy.data.scenes["Scene"].tool_settings.unified_paint_settings.color
-    # bpy.data.scenes["Scene"].tool_settings.unified_paint_settings.color
     """
+    mode = context.object.mode
     tool_settings = context.tool_settings
-    if unified_paint_settings := getattr(tool_settings, "unified_paint_settings"):
+    if mode == "PAINT_GREASE_PENCIL":
+        brush = tool_settings.gpencil_paint.brush
+        return brush
+    elif unified_paint_settings := getattr(tool_settings, "unified_paint_settings"):
         return unified_paint_settings
     return None
 
 
 def get_brush(context):
-    """
-    bpy.data.brushes["Paint Hard", "C:\\Program Files\\Blender Foundation\\Blender 4.4\\4.4\\datafiles\\assets\\brushes\\essentials_brushes-mesh_sculpt.blend"].strength
-    bpy.data.brushes["Average", "C:\\Program Files\\Blender Foundation\\Blender 4.4\\4.4\\datafiles\\assets\\brushes\\essentials_brushes-mesh_weight.blend"].strength
-    """
+    """    """
     mode = context.object.mode
     tool_settings = context.tool_settings
     if mode == "VERTEX_PAINT":  # 在顶点绘制模式下
@@ -40,7 +40,6 @@ def get_brush(context):
     elif mode == "PAINT_GPENCIL":  # 在 Grease Pencil 绘制模式下
         brush = tool_settings.gpencil_paint.brush
         return brush
-
     elif mode == "PAINT_GREASE_PENCIL":
         brush = tool_settings.gpencil_paint.brush
         return brush
@@ -50,4 +49,5 @@ def get_brush(context):
     elif mode == "SCULPT_GREASE_PENCIL":
         brush = tool_settings.gpencil_paint.brush
         return brush
+
     return None
